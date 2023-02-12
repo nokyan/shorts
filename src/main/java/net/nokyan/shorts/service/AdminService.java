34c4aad3dd3e5b2a8f7a72ecac3d65a50b0fb0bc
nokyan <hello@nokyan.net>
2023-02-12 15:13:27 +0100

    Remove unnecessary imports

--- src/main/java/net/nokyan/shorts/service/AdminService.java
+++ src/main/java/net/nokyan/shorts/service/AdminService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import net.nokyan.shorts.model.BlocklistEntry;
